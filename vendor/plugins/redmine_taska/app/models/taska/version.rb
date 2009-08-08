abead4ddfefba411bf1cdae7af107d908533e7b7
Boris Bezrukov <lovchy@Lavanda.local>
2009-08-08 20:25:55 +0400

    Version selection field fixed

--- vendor/plugins/redmine_taska/app/models/taska/version.rb
+++ vendor/plugins/redmine_taska/app/models/taska/version.rb
@@ -3,14 +3,14 @@ module Taska
     def self.included(base)
       base.class_eval do
         acts_as_event :title => Proc.new {|o| o.name},
-                      :author => Proc.new {|o| o.activity_created_by},
+                      :author => Proc.new {|o| o.activity_created_by_id},
                       :datetime => :updated_on,
                       :type => 'version',
                       :url => Proc.new {|o| {:controller => 'versions', :action => 'show', :id => o.id}}
         
         acts_as_activity_provider :type => 'versions',
                                   :timestamp => "#{table_name}.updated_on",
-                                  :author_key => 'activity_created_by',
+                                  :author_key => 'activity_created_by_id',
                                   :find_options => {:include => :project}
       end
     end
